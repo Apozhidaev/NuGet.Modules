b73bec769da7befd9a05d5b25fa491fc09b0db4c
alexanderpo <alexander.pozhidaev@gmail.com>
2016-02-09 08:03:38 +0300

    added OwinRequestExtensions

---
+++ OwinRequestExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+
+// ReSharper disable once CheckNamespace
+namespace NuGet.Modules
+{
+    public static class OwinRequestExtensions
+    {
+        public static string GetIpAddress(this HttpRequestMessage request)
+        {
+            string ipAddress = null;
+            var owinContext = request.GetOwinContext();
+            if (owinContext != null)
+            {
+                ipAddress = request.GetOwinContext().Request.RemoteIpAddress;
+            }
+            return ipAddress;
+        }
+    }
+}
